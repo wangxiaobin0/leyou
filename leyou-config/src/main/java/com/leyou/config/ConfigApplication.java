6dce9e3749af8b12d73fc979e33fb5fc0014a622
wangxb <1378975974@qq.com>
2020-03-19 10:59:05 +0800

    新增配置中心

---
+++ leyou-config/src/main/java/com/leyou/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.leyou.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import sun.security.krb5.Config;
+
+/**
+ * @author
+ * @date 2020/3/19
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
